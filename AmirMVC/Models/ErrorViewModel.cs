7ca87fa15ca00ed670ff523eaf33d0ae30b9eec8
Amir Moemen <amir.moemen@sigma.se>
2018-03-01 16:31:53 +0100

    Initial

---
+++ AmirMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AmirMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
