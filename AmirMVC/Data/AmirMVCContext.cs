7803f0db7d422a6fbf0667a350a353ae7c813bbd
Amir Moemen <amir.moemen@sigma.se>
2018-03-02 12:41:29 +0100

    more changes

---
+++ AmirMVC/Data/AmirMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AmirMVC.Models
+{
+    public class AmirMVCContext : DbContext
+    {
+        public AmirMVCContext (DbContextOptions<AmirMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AmirMVC.Models.Movie> Movie { get; set; }
+    }
+}
